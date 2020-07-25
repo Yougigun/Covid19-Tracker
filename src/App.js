a9053f509b4ac8efe007985bd8a2eaf62d1e1440
Yougigun <s14031403@gmail.com>
2020-07-25 18:59:14 +0800

    Structure of COVID 19 Tracker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">  
+    <h1>Let build a COVID 19 tracker </h1>
+    {/* Header */}
+    {/* Title + Select input dropdown field */}
+    
+    {/* InfoBoxs */}
+    {/* InfoBoxs */}
+    {/* InfoBoxs */}
+    
+    {/* Table */}
+    {/* Graph */}
+
+    {/* Map */}
     </div>
-  );
+  ); 
 }
 
 export default App;
