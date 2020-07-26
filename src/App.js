1d3f32643ec10a8c059ca42e40e423972e7b4192
Yougigun <s14031403@gmail.com>
2020-07-26 08:37:31 +0800

    header already done now work on info box

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+
+} from "@material-ui/core"
+
 import './App.css';
 
 function App() {
-  
-  return (
-    <div className="app">  
-    <h1>Let build a COVID 19 tracker </h1>
-    {/* Header */}
-    {/* Title + Select input dropdown field */}
-    
-    {/* InfoBoxs */}
-    {/* InfoBoxs */}
-    {/* InfoBoxs */}
-    
-    {/* Table */}
-    {/* Graph */}
+  const [countries, setCountries]=useState([])
+  const [country, setCountry] = useState("worldwide")
+  useEffect(()=>{
+    const getCountriesData = async ()=>{
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response)=>response.json())
+      .then(data=>{
+        const countries = data.map(country=>(
+          {
+            name:country.country,
+            value:country.countryInfo.iso2
+          }
+        ))
+        setCountries(countries)
+        
+      })
+    }
+    getCountriesData()
 
-    {/* Map */}
+    // fetch("https://disease.sh/v3/covid-19/countries")
+    //   .then((response) => response.json())
+    //   .then(data => {
+    //         const countries = data.map(country => (
+    //             {
+    //               name: country.country,
+    //               value: country.countryInfo.iso
+    //             }
+    //           )
+    //          )
+    //         setCountries(countries)
+    //         })
+
+  },[])
+  // https://disease.sh/v3/covid-19/countries
+  const onCountryChange = (event)=>{
+    const countryCode = event.target.value
+    console.log("Yooooooo>>>",countryCode)
+    setCountry(countryCode)
+  }
+  return (
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined"
+                  value={country}
+                  onChange={onCountryChange}
+          >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {
+              countries.map(country =><MenuItem value={country.value}>{country.name}</MenuItem>)
+            }
+          </Select>
+        </FormControl>
+      </div>
+      <div className="app__stats">
+          {/* InfoBoxs */}
+          {/* InfoBoxs */}
+          {/* InfoBoxs */}
+      </div>
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
-  ); 
+  );
 }
 
 export default App;
