8c7ca015495da7e17e744a3e04145cbffc0fcbae
Yougigun <s14031403@gmail.com>
2020-07-26 11:20:26 +0800

    build and push to github pages

---
+++ docs/precache-manifest.1681df016c46cc56f47b51e44388cb5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ebeb33915bc86a1a1de49f68e3d9b5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a94e10d155bec2956633",
+    "url": "/static/css/main.19ae446d.chunk.css"
+  },
+  {
+    "revision": "e257ce03335172bc8f9a",
+    "url": "/static/js/2.9344ffdb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9344ffdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a94e10d155bec2956633",
+    "url": "/static/js/main.6a815070.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
